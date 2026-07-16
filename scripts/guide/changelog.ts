b8712f5a549ba129a9943f420db885717b2dc857
Legopitstop <1589lego@gmail.com>
2026-07-16 13:05:40 -0500

    Rename changelogs to changelog

---
+++ scripts/guide/changelog.ts
@@ -0,0 +1,55 @@
+// Generated from changelog.json. Do not edit.
+export const changelog = {
+  changelog: {
+    icon: "textures/ui/creative_icon.png",
+    title: "guide.common.changelogs",
+    body: "guide.common.changelogs.desc",
+    buttons: [
+      "changelog_2_0_0",
+      "changelog_1_7_0",
+      "changelog_1_6_0",
+      "changelog_1_5_0",
+      "changelog_1_4_0",
+      "changelog_1_3_0",
+      "changelog_1_2_1",
+      "changelog_1_2_0",
+      "changelog_1_1_0",
+    ],
+  },
+  changelog_2_0_0: {
+    title: "v2.0.0",
+    body: "2026-00-00 - legopitstop\n\n§lGeneral§r\n- Requires Assets+ v2.0.1\n- Updated for Minecraft 26.21\n- Added compatibility with Vibrant Visuals.\n- Blocks can now be waterlogged.\n- Changed ids to `lpsm_mb`\n\n§lNew§r\n- Added a guide book.",
+  },
+  changelog_1_7_0: {
+    title: "v1.7.0",
+    body: "2024-13-12 - legopitstop\n\n§lGeneral§r\n- Requires Assets+ v1.8.0\n- Updated for 1.21.50\n- Changed `#moreblocks` suffix to `@moreblocks`\n- Added support for any language (translations may not be accurate).\n\n§lNew§r\n- Chiseled Resin Brick Layer\n- Chiseled Resin Brick Slab\n- Chiseled Resin Brick Stairs\n- Chiseled Resin Brick Vertical Slab\n- Pale Moss Block Layer\n- Pale Moss Block Slab\n- Pale Moss Block Stairs\n- Pale Moss Block Vertical Slab\n- Pale Oak Layer\n- Pale Oak Leaves Layer\n- Pale Oak Leaves Slab\n- Pale Oak Leaves Stairs\n- Pale Oak Leaves Vertical Slab\n- Pale Oak Log Layer\n- Pale Oak Log Slab\n- Pale Oak Log Stairs\n- Pale Oak Log Vertical Slab\n- Pale Oak Vertical Slab\n- Pale Oak Wood Layer\n- Pale Oak Wood Slab\n- Pale Oak Wood Stairs\n- Pale Oak Wood Vertical Slab\n- Resin Block Layer\n- Resin Block Slab\n- Resin Block Stairs\n- Resin Block Vertical Slab\n- Resin Brick Layer\n- Resin Brick Vertical Slab\n- Stripped Pale Oak Log Layer\n- Stripped Pale Oak Log Slab\n- Stripped Pale Oak Log Stairs\n- Stripped Pale Oak Log Vertical Slab\n- Stripped Pale Oak Wood Layer\n- Stripped Pale Oak Wood Slab\n- Stripped Pale Oak Wood Stairs\n- Stripped Pale Oak Wood Vertical Slab\n\n§lFixes§r\n- Stripping blocks now require an axe instead of using any item except an axe.\n- Fixed broken and duplicate recipes.\n- Vertical Slab crafting table recipes now give you 6 items instead of 2.\n- Sandstone and Red Sandstone recipe now give the correct item.",
+  },
+  changelog_1_6_0: {
+    title: "v1.6.0",
+    body: "2024-19-11 - legopitstop\n\n§lGeneral§r\n- Requires Assets+ v1.7.0\n- Updated for 1.21.44\n- All blocks now use custom components. See docs for more info.\n- Merged with more stairs and slabs Add-On.\n- Most blocks should behave like their full block counter part. Submit a bug report if this is not the case.\n- Changed up stair hitboxes so you can use walls underneath to make a stair like hitbox.\n- Blocks from moress should get converted to moreblocks",
+  },
+  changelog_1_5_0: {
+    title: "v1.5.0",
+    body: '2023-27-12 - legopitstop\n\n§lGeneral§r\n- Updated for 1.20.51\n- Requires Assets+ v1.6.0\n- Added "Update 1.21" toggle in behavior pack settings.',
+  },
+  changelog_1_4_0: {
+    title: "v1.4.0",
+    body: "2023-07-11 - legopitstop\n\n§lGeneral§r\n- Updated for 1.20.41\n- Increasing layers will now play the sound for all players within 17 blocks.\n- You can now place the layers up side down.\n- Mangrove, Slime, and Honey layers have custom models.",
+  },
+  changelog_1_3_0: {
+    title: "v1.3.0",
+    body: "2023-22-05 - legopitstop\n\n§lGeneral§r\n- Updated for 1.19.80\n- Requres Assets Plus v1.4.0\n\n§lNew§r\n- Added all 1.20 blocks",
+  },
+  changelog_1_2_1: {
+    title: "v1.2.1",
+    body: "2022-11-07 - legopitstop\n\n§lFixes§r\n- The second layer should now use the proper geometry.\n- All glass side textures should use the custom texture to make the sides look better.",
+  },
+  changelog_1_2_0: {
+    title: "v1.2.0",
+    body: "2022-07-01 - legopitstop\n\n§lChanges§r\n- Updated for 1.19\n- Now uses Assets Plus v1.2.0\n\n§lRemoved§r\n- Removed give functions. You can obtain all items through the creative menu.\n- Removed all crafting recipes. You can use the stonecutter instead.\n- Removed concrete shard item\n- Removed terracotta shard item\n- Removed yarn item\n\n§lAdded§r\n- Added TONS of new layer blocks\n- All blocks can be obtained by placing the full block inside a stonecutter",
+  },
+  changelog_1_1_0: {
+    title: "v1.1.0",
+    body: "2022-14-06 - legopitstop\n\n§lGeneral§r\n- min Minecraft version ( 1.180 )\n- Blocks will now show up in the creative inventory.",
+  },
+};
